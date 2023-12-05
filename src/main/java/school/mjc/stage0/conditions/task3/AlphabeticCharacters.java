7ae5423fa39e9f77d89bbd3bf24accbd62aebfb9
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2023-12-05 23:07:06 +0600

    s

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        String str = "aeiouAEIOU";
+        if(str.indexOf(character) != 1){
+            System.out.println("Vowel");
+        }
+        else{
+            System.out.println("Consonant");
+        }
     }
 }
