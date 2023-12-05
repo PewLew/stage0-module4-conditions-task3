2aa4d65691bab2c3ef6a31bd51fb048444e429a8
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2023-12-05 23:15:35 +0600

    s

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,21 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-        float sal;
+        float saal;
         if(salary < 0){
             System.out.println("wrong input!");
         }
         else if(salary > 20000){
-            sal = (float) salary * 80 / 100;
-            System.out.println(sal);
+            saal = (float) salary * 80 / 100;
+            System.out.println(saal);
         }
         else if(salary > 10000 && salary <= 20000){
-            sal = (float) salary * 82 / 100;
-            System.out.println(sal);
+            saal = (float) salary * 82 / 100;
+            System.out.println(saal);
         }
         else if(salary <= 10000){
-            sal = (float) salary * 85 / 100;
-            System.out.println(sal);
+            saal = (float) salary * 85 / 100;
+            System.out.println(saal);
         }
     }
 }
